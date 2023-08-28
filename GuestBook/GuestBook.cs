f2ab6b078b816cf48ec81de2c3487069c3502536
Jesse Pantoja <pantojabruin@gmail.com>
2023-08-28 15:21:16 -0700

    Add helper methods to validate family name and party size are not null

--- GuestBook/GuestBook.cs
+++ GuestBook/GuestBook.cs
@@ -14,50 +14,102 @@ public static class GuestBook
         while (true)
         {
             string? familyName = GetFamilyName();
-            Console.WriteLine($"Entered family name: {familyName}");
-            int? partySize = GetPartySize(familyName);
-            Console.WriteLine($"Entered party size: {partySize}");
+            if (!familyNameIsValid(familyName)) continue;
+            // to do: sanitize familyName
+            //if (doesUserWantToQuit(familyName)) return;
+
+            string? partySizeAsString;
+            do
+            {
+                partySizeAsString = GetPartySize(familyName);
+            } while (!partySizeAsStringIsValid(partySizeAsString));
+
 
         }
     }
 
     private static string? GetFamilyName()
     {
+        PrintAppInstructions();
         Console.Write("What is your family name?: ");
         string? familyName = Console.ReadLine();
 
-        if (familyName is null || familyName == String.Empty)
+        return familyName;
+    }
+
+    private static string? GetPartySize(string? familyName)
+    {
+        PrintAppInstructions();
+        Console.Write($"How many people are in the {familyName} party?: ");
+        string? partySizeAsString = Console.ReadLine();
+
+        return partySizeAsString;
+
+    }
+
+    private static bool familyNameIsValid(string? familyName)
+    {
+        if (familyName is null || familyName.Trim() == String.Empty || familyName.Trim().Length < 2)
         {
-            return null;
+            Console.Clear();
+            Console.WriteLine("ERROR: Your family name must be at least 2 characters! Press the enter key to continue...");
+            Console.ReadLine();
+            return false;
         }
         else
         {
-            return familyName;
+            return true;
         }
     }
 
-    private static int? GetPartySize(string familyName)
+    private static bool partySizeAsStringIsValid(string? partySizeAsString)
     {
-
-        while (true)
+        if (partySizeAsString is null || partySizeAsString.Trim() == String.Empty)
         {
-            Console.Write($"How many people are in the {familyName} party?: ");
-            string? partySize = Console.ReadLine();
-
-
-            // Error check for non-integers
-            if (partySize is null || partySize == String.Empty || !int.TryParse(partySize, out int partySizeAsInt))
-            {
-                Console.WriteLine("You must enter an integer amount for your party size!");
-                continue;
-            }
-            else
-            {
-                return partySizeAsInt;
-            }
-
+            Console.Clear();
+            Console.WriteLine("ERROR: You need to enter an integer party size. Press the enter key to continue...");
+            Console.ReadLine();
+            return false;
+        }
+        else
+        {
+            return true;
         }
     }
+
+    private static void PrintAppInstructions()
+    {
+        Console.Clear();
+        Console.WriteLine("@@@@@ Follow the prompts or type 'exit' to finish building the Guest Book! @@@@");
+        Console.WriteLine("===============================");
+    }
+
+    //private static bool doesUserWantToQuit(string? userInput)
+    //{
+    //    if (userInput is not null && !(userInput.Trim().ToLower() == "exit"))
+    //    {
+    //        return false;
+    //    }
+
+    //    while (true)
+    //    {
+    //        Console.WriteLine("Are you sure you want to exit?");
+    //        Console.Write("Type 'yes' to finish your entries or 'no' to continue building Guest Book: ");
+    //        string? willUserExitResponse = Console.ReadLine();
+
+    //        string[] validResponses = { "yes", "no" };
+
+    //        if (willUserExitResponse is null || Array.IndexOf(validResponses, willUserExitResponse) == -1)
+    //        {
+    //            Console.WriteLine("Error: You must enter either 'yes' if you want to finish or 'no' if you want to continue building the guestbook.");
+    //            continue;
+    //        }
+
+    //        if (userInput == "yes" || userInput == "y") return true;
+    //        else return false;
+
+    //    }
+    //}
 }
 
 

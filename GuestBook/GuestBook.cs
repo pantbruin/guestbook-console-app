dbd5d0a8e01e78baacacbcda27c9fd0420369829
Jesse Pantoja <pantojabruin@gmail.com>
2023-08-28 14:04:37 -0700

    Add helper method to get a family name from a user

--- GuestBook/GuestBook.cs
+++ GuestBook/GuestBook.cs
@@ -1,11 +1,36 @@
 ﻿using System;
-namespace GuestBook
-{
+namespace GuestBookUtilities;
+
 	public static class GuestBook
 	{
 		public static void BuildGuestBook()
 		{
-		}
+        Console.WriteLine("Welcome to Build a Guestbook App!");
+        Console.WriteLine("To begin, push the enter key...");
+        Console.ReadLine();
+        Console.Clear();
+
+        while (true)
+        {
+            string? familyName = GetFamilyName();
+            Console.WriteLine($"Entered: {familyName}");
+
+        }
+    }
+
+		private static string? GetFamilyName()
+		{
+        Console.Write("What is your family name?: ");
+        string? familyName = Console.ReadLine();
+
+        if (familyName is null || familyName == String.Empty)
+        {
+            return null;
+        }
+        else
+        {
+            return familyName;
+        }
+    }
 	}
-}
 
--- GuestBook/Program.cs
+++ GuestBook/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using GuestBookUtilities;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        GuestBook.BuildGuestBook();
+    }
+}
